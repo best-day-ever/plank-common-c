ReenigneArcher/moonlight-common-c

Add NXDK (Xbox) support

nanors/deps/obl/oblas_lite.c
@@ -58,7 +58,7 @@ void obl_axpyb32_ref(u8 *a, u32 *b, u8 u, unsigned k)
     for (unsigned idx = 0, p = 0; idx < k; idx += 8 * sizeof(u32), p++) {
         u32 tmp = b[p];
         while (tmp > 0) {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && !defined(NXDK)
             unsigned long index = 0;
             _BitScanForward(&index, tmp);
             unsigned tz = (unsigned int)index;

best-day-ever/plank-common-c

nanors/deps/obl/oblas_lite.c
@@ -58,7 +58,7 @@ void obl_axpyb32_ref(u8 *a, u32 *b, u8 u, unsigned k)
     for (unsigned idx = 0, p = 0; idx < k; idx += 8 * sizeof(u32), p++) {
         u32 tmp = b[p];
         while (tmp > 0) {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && !defined(__clang__)
             unsigned long index = 0;
             _BitScanForward(&index, tmp);
             unsigned tz = (unsigned int)index;